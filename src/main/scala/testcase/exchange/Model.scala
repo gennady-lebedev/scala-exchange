fa81ba0e292baaf0f7399d12fc62e76d08b3617f
gennady <gennady.lebedev@gmail.com>
2018-04-05 08:16:11 +0300

    add limits on amount and price

--- src/main/scala/testcase/exchange/Model.scala
+++ src/main/scala/testcase/exchange/Model.scala
@@ -5,17 +5,31 @@ import testcase.exchange.BidTypes.{Buy, Sell}
 case class Bid(direction: BidType, amount: Int, price: Int)
 
 object Bid {
+  val minPrice = 100
+  val maxPrice = 10000
+  val minAmount = 1
+  val maxAmount = 1000
+
   def apply(s: String): Bid = {
     val r = raw"([BS]) (\d+) (\d+.?\d{1,2}?)".r
     s match {
-      case r(t, a, p) =>
-        val d = p.toDouble * 100
-        if(d.toInt == d)
-          Bid(BidTypes(t), a.toInt, d.toInt)
-        else
-          throw new RuntimeException(s"Price could be with 2 digits after point, '$p' doesn't fit")
+      case r(t, a, p) => Bid(BidTypes(t), a.toInt, p.toDouble)
       case other => throw new RuntimeException(s"Can't parse '$other', expected Bid like 'B 100 42'")
     }
+  }
+
+  def apply(direction: BidType, amount: Int, price: Double): Bid = {
+    val d = price * 100
+    val p = d.toInt
+    if(p == d)
+      if(p > maxPrice || p < minPrice)
+        throw new RuntimeException(s"Price should be in range from 1 to 100, actual $p")
+      else if(amount > maxAmount || amount < minAmount)
+        throw new RuntimeException(s"Amount should be in range from 1 to 1000, actual $amount")
+      else
+        new Bid(direction, amount, p)
+    else
+      throw new RuntimeException(s"Price could be with 2 digits after point, '$price' doesn't fit")
   }
 }
 
--- src/test/scala/testcase/exchange/BidTest.scala
+++ src/test/scala/testcase/exchange/BidTest.scala
@@ -13,12 +13,28 @@ class BidTest extends WordSpec with Matchers {
     Bid("B 100 12.00") should be (Bid(Buy, 100, 1200))
     Bid("B 100 12.0")  should be (Bid(Buy, 100, 1200))
     Bid("S 100 12.34") should be (Bid(Sell, 100, 1234))
-    Bid("S 100 123.4") should be (Bid(Sell, 100, 12340))
+    Bid("S 100 12.3") should be (Bid(Sell, 100, 1230))
   }
 
   "bid price should fail if more then 2 digits after dot" in {
     an[RuntimeException] should be thrownBy Bid("B 100 12.345")
     an[RuntimeException] should be thrownBy Bid("S 100 12.345678901234567")
+  }
+
+  "bid should fail if price more than 100" in {
+    an[RuntimeException] should be thrownBy Bid("B 100 123.45")
+  }
+
+  "bid should fail if price less than 1" in {
+    an[RuntimeException] should be thrownBy Bid("B 100 0.98")
+  }
+
+  "bid should fail if amount less than 1" in {
+    an[RuntimeException] should be thrownBy Bid("B 0 12")
+  }
+
+  "bid should fail if amount more than 1000" in {
+    an[RuntimeException] should be thrownBy Bid("B 1001 12")
   }
 
   "BidType should be parsed from string" in {
