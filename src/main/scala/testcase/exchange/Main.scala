a4b7660655d39684c4ac2171b8f6c17abbfd7950
gennady <gennady.lebedev@gmail.com>
2018-04-07 16:03:43 +0300

    implement CLI

--- src/main/scala/testcase/exchange/Main.scala
+++ src/main/scala/testcase/exchange/Main.scala
@@ -1,20 +1,37 @@
 package testcase.exchange
 
 import com.typesafe.scalalogging.LazyLogging
-import testcase.exchange.BidTypes._
 
+/**
+  * Print bids in format '<kind> <amount> <price>', like 'B 50 10.12' or 'S 20 99.99'
+  * 'exchange' to find optimal amount and price
+  * 'quit' or 'exit' to exit
+  */
 object Main extends LazyLogging {
   def main(args: Array[String]): Unit = {
-    val exchange = new Exchange
-    exchange.add(Bid(Buy, 100, 10))
-    exchange.add(Bid(Buy, 100, 30))
-    exchange.add(Bid(Buy, 100, 20))
-    exchange.add(Bid(Buy, 100, 40))
-    exchange.add(Bid(Sell, 100, 30))
-    exchange.add(Bid(Sell, 100, 20))
-    exchange.add(Bid(Sell, 100, 40))
-    exchange.add(Bid(Sell, 100, 10))
+    var exchange = new Exchange
 
-    logger.info("Best price and amount {}", exchange.calculate())
+    for (ln <- io.Source.stdin.getLines) ln match {
+      case "quit" | "exit" =>
+        println("Exiting...")
+        sys.exit(0)
+      case "exchange" =>
+        exchange.calculate() match {
+          case (_, 0) =>
+            println("0 n/a")
+            exchange = new Exchange
+          case (p, a) =>
+            println(s"$a ${p.toDouble / 100}")
+            exchange = new Exchange
+          case other =>
+            throw new RuntimeException(s"Unexpected result $other")
+        }
+      case _ =>
+        try {
+          exchange.add(Bid(ln))
+        } catch {
+          case e: Exception => println("not a Bid, try again")
+        }
+    }
   }
 }
